1ecaede9ae310077fa27dff2fe0ee290c54a5048
Robert <robert.jones.1989@outlook.com>
2026-02-03 14:02:46 -0400

    create claim submission page

---
+++ src/app/employee-claim-submit/page.tsx
@@ -0,0 +1,14 @@
+export default function EmployeeClaimSubmit() {
+
+
+    return (
+        <header>
+            <div className="flex items-center gap-4 mb-6">
+                <img src="/images/weyyuLogo.png" alt="Company Logo" width={150} height={150} />
+                <div className="text-3xl font-bold">
+                    Welcome to Weyland-Yutani's Employee Claims Service
+                </div>
+            </div>
+        </header>
+    );
+}
