f44e0893343207e1234f06bf1384a9b619742ee0
Dorantino1 <seth.partnerdeals@gmail.com>
2026-02-08 17:13:27 -0400

    fix: clean up employee folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 // import Login from './employee/Login';
 // import EmployeeClaimForm from './employee-claim-form/page';
-import AddUser from './admin/addUser';
-import ClaimCategory from './employee-claim-category/page';
-import EmployeeClaimForm from './employee-claim-form/page';
-import EmployeeClaimSubmit from './employee-claim-submit/page';
-import ClaimSent from './employee-claim-success/page';
+import AddUser from './admin/add-user/addUser';
+import ClaimCategory from './employee/claim-category/page';
+import EmployeeClaimForm from './employee/claim-form/page';
+import EmployeeClaimSubmit from './employee/claim-submit/page';
+import ClaimSent from './employee/claim-success/page';
 
 export default function Home() {
 
