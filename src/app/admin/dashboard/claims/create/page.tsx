9f468ed79ed705c295513957df4bb19388b2f79a
Dorantino1 <seth.partnerdeals@gmail.com>
2026-04-12 21:28:00 -0300

    add suspense

--- src/app/admin/dashboard/claims/create/page.tsx
+++ src/app/admin/dashboard/claims/create/page.tsx
@@ -1,14 +1,14 @@
 import { getEmployees } from '@/tools/DataManager';
 import CreateClaim from "@/components/adminClaimStart";
-
-
+import { Suspense } from "react";
 
 export default async function page() {
     const users = await getEmployees();
-
     return (
         <>
-            <CreateClaim users={users} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <CreateClaim users={users} />
+            </Suspense>
         </>
     );
 }
